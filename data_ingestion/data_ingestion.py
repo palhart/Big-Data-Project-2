4f0353f5e8aa255d9674fe68a66e6419fea82157
Paul Hartmann <paul.hartmann@epita.fr>
2024-11-06 13:07:31 +0100

    feat(data_ing): update function upload

--- data_ingestion/data_ingestion.py
+++ data_ingestion/data_ingestion.py
@@ -2,10 +2,8 @@ from hdfs import InsecureClient
 import pandas as pd
 
 # Connect to the NameNode's WebHDFS interface
-hdfs_client = InsecureClient('http://localhost:9870', user='root')
+hdfs_client = InsecureClient('http://localhost:9870', user='paul')
 
-# List directories in the HDFS root
-print(hdfs_client.list('/'))
 
 def upload_file_to_hdfs(local_file_path, hdfs_file_path):
     try:
@@ -14,7 +12,5 @@ def upload_file_to_hdfs(local_file_path, hdfs_file_path):
     except Exception as e:
         print(f"Failed to upload {local_file_path} to {hdfs_file_path}: {e}")
 
-# Example usage
-upload_file_to_hdfs('/data/BigData/ecommerce_data_with_trends.csv', 'data.csv')
 
 
