64de7c88f4e1e99ef801d1121a2f188535f2945d
alexandre <alexandre.bailly>
2024-11-29 16:40:21 +0100

    feat(sql_analytics): Added comments to the functions

--- sql_analytics/sql_queries.py
+++ sql_analytics/sql_queries.py
@@ -1,5 +1,5 @@
 def get_top_ten_customer(spark):
-    # Query to find the 100 customers that spent the most
+    # Query to find the top 100 customers based on total amount spent
     query = """
     SELECT customer_id, customer_name, SUM(total_amount) AS total_spent
     FROM transactions
@@ -10,7 +10,7 @@ def get_top_ten_customer(spark):
     return spark.sql(query)
 
 def get_top_five_products(spark):
-    # Query to find the 5 most popular products by quantity sold
+    # Query to find the top 5 most popular products by total quantity sold
     query = """
     SELECT product_name, SUM(quantity) AS total_quantity
     FROM transactions
@@ -18,7 +18,7 @@ def get_top_five_products(spark):
     ORDER BY total_quantity DESC
     LIMIT 5
     """
-    return spark.sql(query) 
+    return spark.sql(query)
 
 def get_top_five_cities(spark):
     # Query to find the top 5 cities with the highest total spending
@@ -30,9 +30,10 @@ def get_top_five_cities(spark):
     ORDER BY total_spent DESC
     LIMIT 5
     """
-    return spark.sql(query) 
+    return spark.sql(query)
 
 def get_most_purchased_products(spark):
+    # Query to find the top 100 most purchased products by total quantity sold
     query = """
     SELECT product_name, SUM(quantity) AS total_quantity 
     FROM transactions 
@@ -40,18 +41,20 @@ def get_most_purchased_products(spark):
     ORDER BY total_quantity DESC
     LIMIT 100
     """
-    return spark.sql(query) 
+    return spark.sql(query)
 
 def get_revenue_by_month(spark):
+    # Query to find the total revenue by month, formatted as yyyy-MM
     query = """
     SELECT DATE_FORMAT(timestamp, 'yyyy-MM') AS month, SUM(total_amount) AS total_revenue 
     FROM transactions 
     GROUP BY month 
     ORDER BY month
     """
-    return spark.sql(query) 
+    return spark.sql(query)
 
 def get_less_purchased_products(spark):
+    # Query to find the top 100 least purchased products by total quantity sold
     query = """
     SELECT product_name, SUM(quantity) AS total_quantity 
     FROM transactions 
@@ -59,9 +62,10 @@ def get_less_purchased_products(spark):
     ORDER BY total_quantity ASC
     LIMIT 100
     """
-    return spark.sql(query) 
+    return spark.sql(query)
 
 def get_revenue_contribution(spark):
+    # Query to find the total revenue contribution by the main category of products
     query = """
     SELECT SUBSTRING_INDEX(category, '>', 1) AS main_category,
     SUM(total_amount) AS revenue
@@ -69,11 +73,10 @@ def get_revenue_contribution(spark):
     GROUP BY main_category
     ORDER BY revenue DESC;
     """
-    return spark.sql(query) 
-
+    return spark.sql(query)
 
 def get_most_purchased_category_by_area(spark, area_name):
-    # Replace with the actual query to get the most purchased category based on area
+    # Query to find the most purchased category in a specific area based on total spending
     query = f"""
     SELECT category, SUM(total_amount) as total_spent
     FROM transactions
@@ -82,5 +85,4 @@ def get_most_purchased_category_by_area(spark, area_name):
     ORDER BY total_spent DESC
     LIMIT 1
     """
-    
-    return spark.sql(query) 
+    return spark.sql(query)
