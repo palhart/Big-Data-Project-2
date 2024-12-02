832c71fd6a98b8e05e2a4996998ffff9406b90de
Nicolas Schmitt <nicola.schmitt@epita.fr>
2024-12-02 22:15:17 +0100

    fix(data_ingestion): added log

--- data_ingestion/data_ingestion.py
+++ data_ingestion/data_ingestion.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
+import logging
 
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def write_to_hdfs(local_path, spark):
     df = spark.read.csv("file:///"  + local_path, header=True)
@@ -12,6 +14,12 @@ if __name__ == "__main__":
     .appName("WriteToHDFS") \
     .config("spark.hadoop.fs.defaultFS", "hdfs://hdfs-namenode:8020") \
     .getOrCreate()
+
+    logger.info("Spark session is successfully connected.")
+
     write_to_hdfs("/data/BigData/ecommerce_data_with_trends.csv", spark)
+
+    logger.info("Data is successfully written to HDFS.")
+
     spark.stop()
     
